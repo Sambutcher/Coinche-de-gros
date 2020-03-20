afb8fcc6ffe108962223e1d6d5337f6d61c25c10
Sambutcher <Samuel.boucher@gmail.com>
2020-03-20 22:35:58 +0100

    MAJ

--- Server/Public/main.js
+++ Server/Public/main.js
@@ -6,31 +6,51 @@ var $mainPage = $('#mainpage');// The main page
 
 
 // Prompt for setting a username
-var username;
-var connected = false;
+var noJoueur;
 
 var socket = io();//connection au socket
 
 // Sets the client's username
 const setUsername = () => {
-  username = cleanInput($('#usernameInput').val().trim());
 
-  // If the username is valid
+  //nettoie le text de l'input
+  const cleanInput = (input) => {
+    return $('<div/>').text(input).html();
+  }
+
+  username = cleanInput($('#joueur'+noJoueur).val().trim());
+
+  //Si le username est ok on l'envoie avec le numéro au serveur et on entre dans la salle
   if (username) {
     $loginPage.hide();
     $mainPage.show();
-
-    // Tell the server your username
-    socket.emit('add user', username);
+    socket.emit('add user', username, noJoueur);
   }
 }
 
-// Prevents input from having injected markup
-const cleanInput = (input) => {
-  return $('<div/>').text(input).html();
-}
 
-//Click sur bouton de login
-$('#Entrer').click(()=>{
+//Evenements Click sur un bouton de login pour entrer dans la salle
+for (let i=1;i<5;i++){
+$('#Entrer'+i).click(()=>{
+  noJoueur=i;
   setUsername();
+})}
+
+
+//evenements du socket//
+
+//Si Salle pleine, changer de page (le socket est déconnecté par le serveur)
+socket.on('sallePleine',()=>{
+  $loginPage.hide();
+  $closePage.show();
+})
+
+//Met à jour la salle d'attente quand le serveur demande
+socket.on('MAJsalle',(salle)=>{
+  for (let i=0;i<4;i++){
+    $('#joueur'+(i+1)).val(salle[i]);
+    if (salle[i]!="") { $('#Entrer'+(i+1)).hide();
+    } else { $('#Entrer'+(i+1)).show();
+    }
+  }
 })
--- Server/app.js
+++ Server/app.js
@@ -7,30 +7,34 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public')); //sert les fichiers clients dans le dossier "public"
 
-var numUsers=0;
+var numUsers=0;//nonmbre dans la salle d'attente
+var salle=["","","",""];//salle de jeu
 
 //connection d'un client sur le socket
 io.on('connection', function(socket){
 
-  var addedUser=false;
+  //refus au bout du 4ème dans la salle d'attente
+  console.log(numUsers);
+  if (numUsers>3) {
+    socket.emit('sallePleine');
+    socket.disconnect(true);
+  } else {++numUsers;};
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
+  //Etat de la salle à la connection du client
+  socket.emit('MAJsalle',salle);
 
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    console.log("Connection: "+socket.username);
+  // Inscription d'un joueur dans la salle
+  socket.on('add user', (username,nojoueur) => {
+    socket.username = username; // sotckage du nom du client
+    salle[nojoueur-1]=username;
+    socket.broadcast.emit('MAJsalle',salle);//on previent les autres
   });
 
- // when the user disconnects.. perform this
+  // Quand quelqu'un se déconnecte
   socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-      console.log("Déconnection: "+socket.username);
-    }
+    --numUsers;
+    salle[salle.indexOf(socket.username)]="";
+    socket.broadcast.emit('MAJsalle',salle);
   });
 });
 
