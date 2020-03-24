9bd14286a2a7820662713f55e5d67685a63bbb02
Sambutcher <Samuel.boucher@gmail.com>
2020-03-24 19:38:06 +0100

    MAJ

--- Server/Public/main.js
+++ Server/Public/main.js
@@ -1,56 +1,118 @@
-var $window = $(window);
+window.addEventListener('load',()=>{
 
-var $loginPage = $('#loginpage'); // The login page
-var $closePage = $('#closepage'); // The close page
-var $mainPage = $('#mainpage');// The main page
+var $loginPage = document.getElementById('loginpage'); // The login page
+var $closePage = document.getElementById('closepage'); // The close page
+var $mainPage = document.getElementById('mainpage');// The main page
 
+const couleurs=["spade","heart","diamond","club"];
+const valeurs=["1","king","queen","jack","10","9","8","7"];
 
-// Prompt for setting a username
-var noJoueur;
+var noJoueur;//numéro de joueur
+var main;//main du joueur
 
 var socket = io();//connection au socket
 
 // Sets the client's username
 const setUsername = () => {
 
-  //nettoie le text de l'input
-  const cleanInput = (input) => {
-    return $('<div/>').text(input).html();
-  }
-
-  username = cleanInput($('#joueur'+noJoueur).val().trim());
-
-  //Si le username est ok on l'envoie avec le numéro au serveur et on entre dans la salle
-  if (username) {
-    $loginPage.hide();
-    $mainPage.show();
+  username = document.getElementById('joueur'+noJoueur).value;
+    $loginPage.style.display="none";
+    $mainPage.style.display="block";
     socket.emit('add user', username, noJoueur);
-  }
-}
 
+}
+//DEBUG: pour inscrire un joueur
+noJoueur=2;
+setUsername();
 
 //Evenements Click sur un bouton de login pour entrer dans la salle
 for (let i=1;i<5;i++){
-$('#Entrer'+i).click(()=>{
-  noJoueur=i;
-  setUsername();
-})}
+  document.getElementById('Entrer'+i).addEventListener('click',()=>{
+    noJoueur=i;
+    setUsername();
+  }
+)}
 
 
 //evenements du socket//
 
 //Si Salle pleine, changer de page (le socket est déconnecté par le serveur)
 socket.on('sallePleine',()=>{
-  $loginPage.hide();
-  $closePage.show();
+  $loginPage.style.display="none";
+  $closePage.style.display="block";
 })
 
 //Met à jour la salle d'attente quand le serveur demande
 socket.on('MAJsalle',(salle)=>{
   for (let i=0;i<4;i++){
-    $('#joueur'+(i+1)).val(salle[i]);
-    if (salle[i]!="") { $('#Entrer'+(i+1)).hide();
-    } else { $('#Entrer'+(i+1)).show();
+    document.getElementById('joueur'+(i+1)).value=salle[i];
+    if (salle[i]!="") { document.getElementById('Entrer'+(i+1)).style.display="none";
+  } else { document.getElementById('Entrer'+(i+1)).style.display='block';
     }
   }
+})
+
+//DEBUG: entrée direte dans la salle pour gagner le login
+$loginPage.style.display="none";
+$mainPage.style.display='block';
+
+//init de l'affichage de la table de jeu
+var canvas = new fabric.Canvas('c');
+canvas.moveCursor='pointer';
+canvas.hoverCursor = 'pointer';
+
+//Création des sprites cartes
+var imagesCartes={};
+var buf;
+buf = new fabric.Image( document.getElementById('back'), {
+    left: 0,
+    top: 0,
+    originX: "center",
+    originY: "center",
+    scaleX:80/169,
+    scaleY:80/169,
+    hasControls:false,
+    hasBorders:false,
+  });
+imagesCartes['back']=buf;
+
+for (let i=0;i<couleurs.length;i++){
+  for (let j=0;j<valeurs.length;j++){
+    buf = new fabric.Image( document.getElementById(couleurs[i]+"_"+valeurs[j]), {
+        left: 0,
+        top: 0,
+        originX: "center",
+        originY: "center",
+        scaleX:80/169,
+        scaleY:80/169,
+        hasControls:false,
+        hasBorders:false,
+        selectable:true,
+      });
+      imagesCartes[couleurs[i]+"_"+valeurs[j]]=buf;
+  }
+}
+
+
+var main;//=[{Valeur:"1",Couleur:"heart"},{Valeur:"king",Couleur:"spade"}]//a mettre à jour
+
+//Afficher la main du joueur en cours
+function afficherMain(){
+    var sousmain=new fabric.Rect({  width: 800, height: 120, fill: 'green', left: 0, top: 480,selectable:false});
+    var soustable=new fabric.Rect({  width: 600, height: 400, fill: 'darkgreen', left: 100, top: 50,selectable:false});
+    canvas.add(sousmain);
+    canvas.add(soustable);
+    socket.emit('pingMain', (data)=>{
+      main=data;
+      for (let i=0;i<main.length;i++){
+          var imagecarte=imagesCartes[main[i].Couleur+"_"+main[i].Valeur];
+          imagecarte.set({left:100+i*90,top:540});
+          canvas.add(imagecarte);
+        }
+    });
+
+  }
+
+  afficherMain();
+
 })
--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,5 @@
 
-//init du serveur http et du socket
+//init du serveur http et du socket (nécessite d'installer express et socket sur le serveur node)
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
@@ -14,7 +14,6 @@ var salle=["","","",""];//salle de jeu
 io.on('connection', function(socket){
 
   //refus au bout du 4ème dans la salle d'attente
-  console.log(numUsers);
   if (numUsers>3) {
     socket.emit('sallePleine');
     socket.disconnect(true);
@@ -26,6 +25,7 @@ io.on('connection', function(socket){
   // Inscription d'un joueur dans la salle
   socket.on('add user', (username,nojoueur) => {
     socket.username = username; // sotckage du nom du client
+    socket.nojoueur=nojoueur;
     salle[nojoueur-1]=username;
     socket.broadcast.emit('MAJsalle',salle);//on previent les autres
   });
@@ -36,6 +36,11 @@ io.on('connection', function(socket){
     salle[salle.indexOf(socket.username)]="";
     socket.broadcast.emit('MAJsalle',salle);
   });
+
+  //Envoi la main quand on lui demande
+  socket.on('pingMain',(fn)=>{
+    fn(table["joueur"+socket.nojoueur].main);
+  })
 });
 
 
@@ -43,3 +48,81 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+//Routine du jeu
+
+//caractéristiques de cartes
+const couleurs=["spade","heart","diamond","club"];
+const valeurs=["1","king","queen","jack","10","9","8","7"];
+
+
+//constructeur tas de cartes mélangé et méthode de coupe
+function Jeu(){
+  var jeu=new Array(); //creation du jeu (liste de cartes)
+//Associer les 32 cartes
+  for (let i=0;i<couleurs.length;i++){
+    for (let j=0;j<valeurs.length;j++){
+      var carte={Valeur:valeurs[j], Couleur:couleurs[i]};
+      jeu.push(carte);
+    }
+  }
+    //Mélanger le jeu
+    for (let i = jeu.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [jeu[i], jeu[j]] = [jeu[j], jeu[i]];
+    }
+
+    //Couper le jeu
+    function couperJeu(j){
+      const r=Math.floor(Math.random() * 32);
+      var res=[];
+      for (let i=0;i<j.length;i++){
+        res[i]=j[(i+r)%32];
+      }
+      return res;
+    }
+
+  this.liste=jeu;
+  this.couperJeu=couperJeu(this.liste);
+
+}
+
+//constructeur joueur (avec un nom et une main)
+function Joueur(prenom){
+  this.nom=prenom;
+  this.main=[];
+}
+
+//construcuteur Table de 4 joueurs et un donneur
+function Table(nom1,nom2,nom3,nom4){
+
+  //Distribuer les cartes aux joueurs
+  function distribuer(jj){
+    jj.couperJeu;
+    var j=jj.liste;
+    for (let i=0;i<4;i++){
+        this["joueur"+(this.donneur+i+1)%4].main.push(j[3*i],j[3*i+1],j[3*i+2]);
+        this["joueur"+(this.donneur+i+1)%4].main.push(j[3*i+12],j[3*i+13],j[3*i+14]);
+        this["joueur"+(this.donneur+i+1)%4].main.push(j[2*i+24],j[2*i+25]);
+    }
+
+  }
+
+  function tourner(){
+    this.donneur=(this.donneur+1)%4;
+  }
+
+  this.joueur0= new Joueur(nom1);
+  this.joueur1= new Joueur(nom2);
+  this.joueur2= new Joueur(nom3);
+  this.joueur3= new Joueur(nom4);
+  this.donneur= 0;
+  this.distribuer=distribuer;
+  this.tourner=tourner;
+}
+
+
+
+var jeu=new Jeu();//on bat les cartes
+var table=new Table("Filou","Cochonou","Shokooh","Sam");//TODO: mettre les joueurs connectés
+table.distribuer(jeu);
